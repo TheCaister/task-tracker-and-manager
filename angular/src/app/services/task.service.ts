a1e6ecea73942c7db633c018b0471b8268d903e2
Edison Cai <edisoncai2001@gmail.com>
2021-07-21 11:50:57 +0100

    configuring getTasks methods so that now it fetches tasks from the server instead of a mock file of tasks

--- angular/src/app/services/task.service.ts
+++ angular/src/app/services/task.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
-import {Observable, of} from 'rxjs'
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
 import { Task } from '../Task';
-import { TASKS } from '../mock-tasks';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks';
 
-  constructor() { }
+  // Adding HTTP client into constructor
+  constructor(private http: HttpClient) { }
 
+  // Getting tasks from json server using HTTP request
   getTasks(): Observable<Task[]> {
-    // of() turns a thing into an observable
-    const tasks = of(TASKS);
-    return tasks;
+    // Must specify type of Observable
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
